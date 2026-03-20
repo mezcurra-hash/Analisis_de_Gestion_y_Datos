720cfc18bd6b835319afed0a916cd259c847fd89
mezcurra-hash <mezcurra@cemic.edu.ar>
2026-03-20 08:33:52 -0300

    Update URL for loading data from Google Sheets

--- app.py
+++ app.py
@@ -746,7 +746,7 @@ elif app_mode == "🎧  Call Center":
 
     @st.cache_data(ttl=300)
     def cargar_datos_redes():
-        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQHFwl-Dxn-Rw9KN_evkCMk2Er8lQqgZMzAtN4LuEkWcCeBVUNwgb8xeIFKvpyxMgeGTeJ3oEWKpMZj/pub?gid=734059738&single=true&output=csv"
+        url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTOxpr7RRNTLGO96pUK8HJ0iy2ZHeqNpiR7OelleljCVoWPuJCO26q5z66VisWB76khl7Tmsqh5CqNC/pub?output=csv"
         try:
             df = pd.read_csv(url)
         except Exception as e:
